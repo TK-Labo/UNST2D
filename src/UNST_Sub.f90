87e8cd60df94991120f4c311520162f52bffe726
baba-gh <tz12dbaba@ec.usp.ac.jp>
2025-07-10 08:25:07 +0900

    Update UNST_Sub.f90
    Integration of UNST_infilt.f90 25.07.10

--- src/UNST_Sub.f90
+++ src/UNST_Sub.f90
@@ -721,3 +721,34 @@ subroutine sumqa(sv, sa, saj)
     !$omp end parallel do
 
 end subroutine sumqa
+
+! infiltration  2507 unst_infilt.f90 統合
+subroutine unst_infilt(me)
+    use globals
+    implicit none
+    
+    real(8) unst_gampt_ff_temp
+    integer me
+    
+    unst_gampt_f(me) = 0.d0
+    unst_gampt_ff_temp = unst_gampt_ff(me)
+    if( unst_gampt_ff_temp .le. 0.01d0 ) unst_gampt_ff_temp = 0.01d0
+
+    ! unst_gampt_f(me) : infiltration capacity [m/s]
+    ! unst_gampt_ff : accumulated infiltration depth [m]
+    unst_gampt_f(me) = unst_ksv(me) * (1.d0 + unst_faif(me) * unst_gammaa(me) / unst_gampt_ff_temp)
+
+    ! unst_gampt_f(me) : infiltration capacity -> infiltration rate [m/s]
+    if( unst_gampt_f(me) .ge. unsth(me) / unstdt ) unst_gampt_f(me) = unsth(me) / unstdt
+
+    ! unst_gampt_ff should not exceeds a certain level
+    if( unst_infilt_limit(me) .ge. 0.d0 .and. unst_gampt_ff(me) .ge. unst_infilt_limit(me) ) unst_gampt_f(me) = 0.d0
+
+    ! update unst_gampt_ff [m]
+    unst_gampt_ff(me) = unst_gampt_ff(me) + unst_gampt_f(me) * unstdt
+
+    ! hs : hs - infiltration rate * dt [m]
+    unsth(me) = unsth(me) - unst_gampt_f(me) * unstdt
+    if( unsth(me) .le. 0.d0 ) unsth(me) = 0.d0
+    
+end subroutine unst_infilt
